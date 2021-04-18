466ae699878670b0588fc32e2ce4448fb42f9b34
Korawit Rittisit <korawit.oum@mail.kmutt.ac.th>
2021-04-19 05:46:22 +0700

    62130500006 [add] view Home and feature Login

---
+++ src/components/Login.vue
@@ -0,0 +1,55 @@
+<template>
+<div>
+<block-content>
+        <form @submit.prevent="login">
+        <h1 class="text-3xl mb-5"> Login </h1>
+        <div class="">
+        <label class="label" for="username">Username</label>
+        <input
+            class="border-2 ml-2"
+            type="text"
+            id="username"
+            v-model="enteredUsername"
+            required /></div>
+        <div class="pt-2 ">
+        <label class="label" for="password">Password</label>
+        <input
+            class="border-2 ml-2"
+            type="password"
+            id="password"
+            v-model="enteredPassword" 
+            required/></div>
+          <div class="space-y-2 py-2 flex-col flex items-end">
+        <button type="submit" class="bg-transparent hover:bg-green-500 text-green-500 font-semibold hover:text-white py-2 px-2 border border-green-500 hover:border-transparent rounded">Login</button>
+        <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-2 border border-blue-500 hover:border-transparent rounded"><router-link to="/register">Register</router-link></button>       
+          </div>
+        </form>
+</block-content>
+</div>
+</template>
+
+<script>
+export default {
+  emits: ['login-user'],
+  data() {
+    return {
+      enteredUsername: '',
+      enteredPassword: '',
+    }
+  },
+  methods: {
+    login() {
+      
+      const userLogin = {
+        username: this.enteredUsername,
+        password: this.enteredPassword
+      }
+      this.enteredUsername = ''
+      this.enteredPassword = ''
+      this.$emit('login-user', userLogin)
+      
+    },
+    
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,109 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home mt-40">
+    <login v-if="!login" @login-user='validateUsernamePassword'></login>
+    <information v-else :info='infoResults' @edited-info='editInfo' @delete-user='deleteId'> </information>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Login from '@/components/Login.vue'
+import Information from '../components/Infomation.vue'
 
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
+    Login,
+    Information
+  },
+data(){
+  return{
+    url: ' http://localhost:5000/infoResults',
+    infoResults: [],
+    login: false,
   }
+},
+methods: {
+  async fetchUsernamePassword(){
+    try{
+      const res = await fetch(this.url)
+      const data = await res.json()
+      return data
+    } catch (error){
+      console.log(error)
+    }
+  },
+  validateUsernamePassword(UsernamePassword){
+      console.log(UsernamePassword.username)
+    
+      this.infoResults = this.infoResults.filter((info) => info.username == UsernamePassword.username 
+      && info.password == UsernamePassword.password)
+      if(this.infoResults.length !== 0){
+      if(this.infoResults[0].username == UsernamePassword.username 
+      && this.infoResults[0].password == UsernamePassword.password){
+        alert('Login Success')
+        this.login = true
+      }}
+      else{
+      alert('Login fail')
+      location.reload();
+      }
+      console.log(this.infoResults)
+      },
+
+      // edit(){
+      //       const editInfo = {
+      //           username: this.infoResults[0].username,
+      //           password: this.infoResults[0].password,
+      //           firstname: this.infoResults[0].firstname,
+      //           lastname: this.infoResults[0].lastname,
+      //           nickname: this.infoResults[0].nickname
+      //       }
+      //       this.$emit('edit-info', editInfo)
+      //   },
+
+    async editInfo(editingData) {
+      const res = await fetch(`${this.url}/${this.infoResults[0].id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          username: this.infoResults[0].username,
+          password: editingData.password,
+          firstname: editingData.firstname,
+          lastname: editingData.lastname,
+          nickname: editingData.nickname
+        })
+      })
+      const data = await res.json()
+      this.infoResults = this.infoResults.map((info) =>
+        info.id === info.id
+          ? {
+              ...info,
+              username: data.username,
+              password: data.password,
+              firstname: data.firstname,
+              lastname: data.lastname,
+              nickname: data.nickname
+            }
+          : info
+      )
+    },
+    async deleteId() {
+        const res = await fetch(`${this.url}/${this.infoResults[0].id}`, {
+          method: 'DELETE'
+        })
+        res.status === 200
+          ? (this.infoResults = this.infoResults.filter(
+              (info) => info.id !== this.infoResults[0].id
+            ))
+          : alert('Error to delete survey')
+      
+    },
+},
+async created() {
+    this.infoResults = await this.fetchUsernamePassword()
+  }
+
 }
 </script>
