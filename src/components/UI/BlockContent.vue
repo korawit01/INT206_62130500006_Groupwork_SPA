e93d5d2d738913e4e5f7254956304bb6669c9e60
Korawit Rittisit <korawit.oum@mail.kmutt.ac.th>
2021-04-19 05:54:59 +0700

    62130500006 [add] global componets, router

---
+++ src/components/UI/BlockContent.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="flex flex-col items-center">
+        <div class="border w-auto p-5 shadow bg-blue-100">
+            <slot></slot>
+        </div>
+    </div>
+</template>
